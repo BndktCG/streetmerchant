c1c6d98b420563cde89398309f4e2a3a39e29acc
Benedikt Gilbert <56096576+BndktCG@users.noreply.github.com>
2025-01-21 15:09:51 +0100

    fixed asus-de buttons

--- src/store/model/asus-de.ts
+++ src/store/model/asus-de.ts
@@ -4,16 +4,21 @@ export const AsusDe: Store = {
   currency: '€',
   labels: {
     inStock: {
-      container: '.buybox--button',
+      container: '#productDetailPageBuyProductForm > div > div:nth-child(11) > button.btn.btn-primary.btn-block.btn-buy',
       text: ['in den warenkorb'],
     },
+    maxPrice: {
+      container: 'body > main > div:nth-child(2) > div > div.product-detail > div.product-detail-buy > div.js-magnifier-zoom-image-container > div.detail-offers > div.product-detail-price-container > p',
+      euroFormat: true,
+    },
   },
+
   links: [
     {
       brand: 'test:brand',
       model: 'test:model',
       series: 'test:series',
-      url: 'https://webshop.asus.com/de/komponenten/grafikkarten/nvidia-serie/2766/asus-rog-strix-rtx2060s-o8g-evo-v2-gaming',
+      url: 'https://webshop.asus.com/de/Grafikkarten/Nvidia-RTX-40-SUPER/ASUS-TUF-Gaming-GeForce-RTX-4070-Ti-SUPER-16GB-GDDR6X-OC-Edition-Gaming-Grafikkarte/90YV0KF0-M0NA00',
     },
     {
       brand: 'asus',
